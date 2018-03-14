10df8d01781c1b735a336ce033b7ad240d474ea7
joalmopa <joalmopa18@gmail.com>
2018-03-13 20:02:19 -0500

    Modified Location src/main/java/co/com/education/config/EducationSystemApplication.java file

--- src/main/java/co/com/education/config/EducationSystemApplication.java
+++ src/main/java/co/com/education/config/EducationSystemApplication.java
@@ -1,10 +1,7 @@
-import lombok.extern.java.Log;
+package co.com.education.config;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class EducationSystemApplication {
