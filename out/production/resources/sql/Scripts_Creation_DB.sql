b6e93ce914e55162e0f6b4d1f226104119e4dd39
joalmopa <joalmopa18@gmail.com>
2018-08-16 18:32:04 -0500

    Added Optional in TeacherGatewayImpl.java class, toCore method

---
+++ out/production/resources/sql/Scripts_Creation_DB.sql
@@ -0,0 +1,89 @@
+DROP TABLE SUBJECT_X_GROUP_X_TEACHER;
+DROP TABLE STUDENTS_X_GROUP;
+DROP TABLE GROUPS;
+DROP TABLE STUDENTS;
+DROP TABLE SUBJECTS;
+DROP TABLE TEACHERS;
+-- DROP SCHEMA 'educationsystem';
+
+-- CREATE SCHEMA 'educationsystem';
+
+CREATE TABLE TEACHERS
+(
+	ID           	  INT 			NOT NULL AUTO_INCREMENT,
+	DOC_TYPE	  	  VARCHAR(2)	NOT NULL,
+	DOC_NUMBER		  VARCHAR(11)	NOT NULL,
+	NAME	      	  VARCHAR(50)   NOT NULL,
+	PHONE	      	  VARCHAR(15)   NOT NULL,
+	EMAIL       	  VARCHAR(50)   NOT NULL,
+	BIRTH_DATE 		  DATE          NOT NULL,
+	GENDER            VARCHAR(1)    NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+-- ============================================================
+--   TABLE: SUBJECTS
+-- ============================================================
+CREATE TABLE SUBJECTS
+(
+	ID           	INT 			NOT NULL AUTO_INCREMENT,
+	DESCRIPTION     VARCHAR(50)   	NOT NULL,
+	CONSTRAINT SUBJECTS PRIMARY KEY (ID)
+);
+
+-- ============================================================
+--   TABLE: STUDENTS
+-- ============================================================
+CREATE TABLE STUDENTS
+(
+	ID                	INT					  NOT NULL AUTO_INCREMENT,
+	DOC_TYPE	  	    VARCHAR(2)		NOT NULL,
+	DOC_NUMBER		    VARCHAR(11)		NOT NULL,
+	NAME      		    VARCHAR(50)   NOT NULL,
+	PHONE	      	    VARCHAR(15)   NOT NULL,
+	EMAIL       	    VARCHAR(50)   NOT NULL,
+	BIRTH_DATE 		    DATE         	NOT NULL,
+	GENDER          	VARCHAR(1)    NOT NULL,
+	PRIMARY KEY (ID)
+);
+
+-- ============================================================
+--   TABLE: GROUPS
+-- ============================================================
+CREATE TABLE GROUPS
+(
+  ID           		INT				NOT NULL AUTO_INCREMENT,
+  DESCRIPTION     	VARCHAR(50)   	NOT NULL,
+  OPEN_YEAR	    	VARCHAR(4)      NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+-- ============================================================
+--   TABLE: STUDENTS_X_GROUP
+-- ============================================================
+CREATE TABLE STUDENTS_X_GROUP
+(
+	ID           	INT				NOT NULL AUTO_INCREMENT,
+	ID_GROUP      	INT             NOT NULL,
+	ID_STUDENT    	INT             NOT NULL,
+	MARK       		DECIMAL(4,2)    NULL,
+  CONSTRAINT STUDENTS_X_GROUP PRIMARY KEY (ID),
+	FOREIGN KEY (ID_GROUP) REFERENCES GROUPS(ID),
+	FOREIGN KEY (ID_STUDENT) REFERENCES STUDENTS(ID)
+);
+
+
+-- ============================================================
+--   TABLE: SUBJECT_X_GROUP_X_TEACHER
+-- ============================================================
+CREATE TABLE SUBJECT_X_GROUP_X_TEACHER
+(
+	ID           	INT				NOT NULL AUTO_INCREMENT,
+	ID_SUBJECT    	INT             NOT NULL,
+    ID_GROUP      	INT             NOT NULL,
+    ID_TEACHER    	INT             NOT NULL,
+  CONSTRAINT SUBJECT_X_GROUP_X_TEACHER PRIMARY KEY (ID),
+	FOREIGN KEY (ID_SUBJECT) REFERENCES SUBJECTS(ID),
+    FOREIGN KEY (ID_GROUP) REFERENCES GROUPS(ID),
+	FOREIGN KEY (ID_TEACHER) REFERENCES TEACHERS(ID)
+);
--- src/main/java/co/com/education/database/gatewayimpl/TeacherGatewayImpl.java
+++ src/main/java/co/com/education/database/gatewayimpl/TeacherGatewayImpl.java
@@ -19,67 +19,52 @@ public class TeacherGatewayImpl implements TeacherService {
 
     @Override
     public List<Teacher> getTeachers() {
-        return  teacherRepository.findAll().stream().map(this::toCore).collect(Collectors.toList());
+        return teacherRepository.findAll().stream()
+            .map(this::toCore)
+            .collect(Collectors.toList());
     }
 
     @Override
     public Teacher getTeacherById(Integer teacherId) {
-        return toCoreOptional(Optional.ofNullable(teacherRepository.findOne(teacherId)));
+        return toCore(teacherRepository.findOne(teacherId));
     }
+
 
     @Override
     public Teacher saveOrUpdateTeacher(Teacher teacher) {
-        return toCoreOptional(Optional.ofNullable(teacherRepository.save(toEntity(teacher))));
+        return toCore(teacherRepository.save(toEntity(teacher)));
     }
+
 
     @Override
     public void deleteTeacher(Integer teacherId) {teacherRepository.delete(teacherId);}
 
+
     @Override
     public List<Teacher> getTeachersByGender(String gender) {
-         return  teacherRepository.findAll().stream().map(this::toCore)
-                 .filter(t -> t.getGender().equals(gender))
-                 .collect(Collectors.toList());
+         return teacherRepository.findAll().stream()
+             .map(this::toCore)
+             .filter(t -> t.getGender().equals(gender))
+             .collect(Collectors.toList());
     }
 
-
-    public Teacher toCoreOptional(Optional<TeacherEntity>  op){
-        if(op.isPresent()){
-            return Teacher.builder()
-                    .id(op.get().getId())
-                    .name(op.get().getName())
-                    .email(op.get().getEmail())
-                    .documentNumber(op.get().getDocumentNumber())
-                    .documentType(op.get().getDocumentType())
-                    .birthDate(op.get().getBirthDate())
-                    .phone(op.get().getPhone())
-                    .gender(op.get().getGender())
-                    .build();
-
-        }else{
-            System.out.println(" ******* no hay datos *********** ");
-            return null;
-        }
-
-
-    }
 
     public Teacher toCore(TeacherEntity entity) {
-
-        return Teacher.builder()
-                .id(entity.getId())
-                .name(entity.getName())
-                .email(entity.getEmail())
-                .documentNumber(entity.getDocumentNumber())
-                .documentType(entity.getDocumentType())
-                .birthDate(entity.getBirthDate())
-                .phone(entity.getPhone())
-                .gender(entity.getGender())
-                .build();
+        return Optional.ofNullable(Teacher.builder()
+            .id(entity.getId())
+            .name(entity.getName())
+            .email(entity.getEmail())
+            .documentNumber(entity.getDocumentNumber())
+            .documentType(entity.getDocumentType())
+            .birthDate(entity.getBirthDate())
+            .phone(entity.getPhone())
+            .gender(entity.getGender())
+            .build())
+        .orElse(Teacher.builder().build());
     }
 
-    public TeacherEntity toEntity(Teacher core) {
 
+    public TeacherEntity toEntity(Teacher core) {
         return new TeacherEntity(core.getId(), core.getDocumentType(), core.getDocumentNumber(), core.getName(),
                 core.getPhone(),core.getEmail(),core.getBirthDate(), core.getGender());
     }
